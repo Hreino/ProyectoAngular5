e0a9ea2e7384d72f45b0d20eb6da32c15f89c74e
Hreino <39251671+Hreino@users.noreply.github.com>
2018-09-08 22:19:32 -0600

    Fin de la seccion 6

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductoInterface } from '../interfaces/producto.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
+  cargando = true;
 
-  constructor() { }
+  productos: ProductoInterface[] = [];
+  constructor( private http: HttpClient ) {
+    this.cargarProductos();
+  }
+
+
+  private cargarProductos() {
+    this.http.get('https://angular-html-c29f0.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: ProductoInterface[]) => {
+      console.log(resp);
+
+
+      setTimeout(() => {
+        this.productos = resp;
+        this.cargando = false;
+      }, 1000); // Delay estatico de un segundo
+
+    } );
+  }
 }
