ade4b395e3e92b701a5bbe15fcfe171df2a5cf38
Hreino <39251671+Hreino@users.noreply.github.com>
2018-09-08 21:04:52 -0600

    servicio de productos

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,18 +8,28 @@ import { HttpClient } from '@angular/common/http';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
-
+  equipo: any [] = [];
   constructor( private http: HttpClient) {
 
-    console.log('Servicio de pagina cargado');
-
+    // console.log('Servicio de pagina cargado');
+      this.cargarInfo();
+      this.cargarEquipo();
+   }
+private cargarInfo() {
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-        // Obteniendo la propiedad especifica del objeto js que genera el JSON
-      });
-   }
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+      // Obteniendo la propiedad especifica del objeto js que genera el JSON
+ });
+}
+
+private cargarEquipo() {
+  this.http.get('https://angular-html-c29f0.firebaseio.com/equipo.json')
+  .subscribe( (resp: any[]) => {
+   this.equipo = resp;
+    // console.log(resp);
+});
+}
 }
